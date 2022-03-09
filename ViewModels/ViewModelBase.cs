d021b657d35820b010e3bf777ec08fdf91fb3377
Anna Lugovaya <annaly4499@gmail.com>
2022-03-09 21:58:31 +0700

    Add project

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4_2.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
